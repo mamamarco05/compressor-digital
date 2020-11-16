378f5f2e6fc95b152a00e4e0e1eb544e0c2d4f47
mamamarco05 <62773244+mamamarco05@users.noreply.github.com>
2020-11-16 19:50:54 -0300

    Add files via upload
    primeiro commit

---
+++ Led/src/Led.cpp
@@ -0,0 +1,11 @@
+/* *********************************************************************** */
+/* File name:        Led.cpp                                               */
+/* File description: File containing the prototypes of the functions used  */
+/*                   to configure and use LEDS                             */
+/* Author name:      Marco A D'Angelo, Rennan Y Nishimori, Tiago R Menezes */
+/* Creation date:    08nov2020                                             */
+/* Revision date:    08nov2020                                             */
+/* *********************************************************************** */
+
+/* includes */
+#include "Led.h"
---
+++ Led/src/Led.h
@@ -0,0 +1,40 @@
+/* *********************************************************************** */
+/* File name:        Led.h                                                 */
+/* File description: Header file containing the prototypes of the functions*/
+/*                   used to configure and use LEDS                        */
+/* Author name:      Marco A D'Angelo, Rennan Y Nishimori, Tiago R Menezes */
+/* Creation date:    08nov2020                                             */
+/* Revision date:    08nov2020                                             */
+/* *********************************************************************** */
+
+#ifndef _LED_H
+#define _LED_H
+
+/* includes */
+
+/* definitions */
+
+/* class */
+class Led
+{
+    private:
+
+    public:
+        /* ************************************************ */
+        /* Method name:        led_init                     */
+        /* Method description: Initialize the LED device    */
+        /* Input params:       iPin                         */
+        /* Output params:      n/a                          */
+        /* ************************************************ */
+        void led_init(int iPin);
+
+        /* ************************************************ */
+        /* Method name:        led_set                      */
+        /* Method description: Set status of specified LED  */
+        /* Input params:       iPin, ucStatus               */
+        /* Output params:      n/a                          */
+        /* ************************************************ */
+        void led_set(int iPin, unsigned char ucStatus);
+}
+
+#endif //_LED_H

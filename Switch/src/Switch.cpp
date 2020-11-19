d819c664fea5ab7fd7c4d3adc83cd0a845706b7d
tiagomenezess <timenezes.97@gmail.com>
2020-11-19 15:07:02 -0300

    teste botoes

--- Switch/src/Switch.cpp
+++ Switch/src/Switch.cpp
@@ -8,12 +8,56 @@
 /* *********************************************************************** */
 
 /* includes */
-#include "Arduino.h"
 #include "Switch.h"
 
 /* functions */
 
-void Switch::switch.init(int iPin){
+void Switch::switch.init(int iPin, int nButton, int sButton){
     pinMode(iPin, INPUT_PULLUP);
     _iPin = iPin;    
+    _nButton = nButton;
+    _sButton = sButton;
+}
+
+int Switch::switch.getStatus(){
+    return _nButton;
+}
+
+int Switch::switch.getPin(){
+    return _iPin;
+}
+
+int Switch::switch.getStatus(){
+    return _sButton;
+}
+
+void Switch::switch.setStatus( int sButton){
+    _sButton = sButton;
+}
+
+void Switch::switch.read(){
+
+    while(true){
+        if (digitalRead(button1.getPin())){
+            button1.setStatus(ON);
+        }
+        else if (!(digitalRead(button1.getPin()))){
+            button1.setStatus(OFF);
+        }
+        if (digitalRead(button2.getPin())){
+            button1.setStatus(ON);
+        }
+        else if (!(digitalRead(button2.getPin()))){
+            button1.setStatus(OFF);
+        }
+        if (digitalRead(button3.getPin())){
+            button1.setStatus(ON);
+        }
+        else if (!(digitalRead(button3.getPin()))){
+            button1.setStatus(OFF);
+        }
+            delay(10);
+
+    }
+
 }
--- Switch/src/Switch.h
+++ Switch/src/Switch.h
@@ -10,8 +10,12 @@
 #ifndef _SWITCH_H
 #define _SWITCH_H
 
+#define ON							1 
+#define OFF							0
+
 /* includes */
-#include "Arduino.h"
+#include ">Arduino.h"
+#include <Wire.h>
 /* definitions */
 
 /* class */
@@ -19,23 +23,51 @@ class Switch
 {
     private:
         int _iPin;
+        int _nButton; //button identifier: 1, 2 ou 3
+        int _sButton; //status of the button, 0 means off, 1 means on
 
     public:
         /* ************************************************** */
         /* Method name:        switch_init                    */
         /* Method description: Initialize the switch device   */
-        /* Input params:       iPin                           */
+        /* Input params:       iPin, nButton, sButton         */
         /* Output params:      n/a                            */
         /* ************************************************** */
-        void switch_init(int iPin);
+        void switch_init(int iPin, int nButton, int sButton);
 
         /* ************************************************** */
-        /* Method name:        switch_get                     */
+        /* Method name:        switch_getStatus               */
         /* Method description: Get status of specified switch */
-        /* Input params:       iPin                           */
-        /* Output params:      ucStatus                       */
+        /* Input params:       None, depends on object        */
+        /* Output params:      sButton                        */
         /* ************************************************** */
-        unsigned char switch_get(int iPin);
+        int switch_getStatus();
+
+        /* ************************************************** */
+        /* Method name:        switch_getPin               */
+        /* Method description: Gets pin of specified switch */
+        /* Input params:       None, depends on object        */
+        /* Output params:      iPin                        */
+        /* ************************************************** */
+        int switch_getPin();
+
+        /* ************************************************** */
+        /* Method name:        switch_getButton               */
+        /* Method description: Gets identifier button number of specified switch */
+        /* Input params:       None, depends on object        */
+        /* Output params:      nButton                        */
+        /* ************************************************** */
+        int switch_getButton();
+
+        /* ************************************************** */
+        /* Method name:        switch_read                    */
+        /* Method description: Ongoing task that reads buttons 
+                               and alters buttons' status. Buttons need to be 
+                               instanced objects in main whose names are defined in this function */
+        /* Input params:       &nButton (address)                     */
+        /* Output params:      None                        */
+        /* ************************************************** */
+        void switch_read( void * pvParameters );
 };
 
 #endif //_SWITCH_H
